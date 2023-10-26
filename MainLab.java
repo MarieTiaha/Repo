3c21f765c3ded40b29733e2b3940fab8ac745018
MarieTiaha <112561373+MarieTiaha@users.noreply.github.com>
2023-10-25 21:56:20 -0400

    Add files via upload

---
+++ MainLab.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class MainLab {
+	public static void main( String [] args )
+    {
+		TwoDimArrayPractice app = new TwoDimArrayPractice( );
+		app.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+    }
+
+}
